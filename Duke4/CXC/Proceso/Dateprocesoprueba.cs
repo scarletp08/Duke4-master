a04d5606dba29212cca62ec1a4f9b747803a034a
scarletp08 <52163927+scarletp08@users.noreply.github.com>
2019-06-27 15:33:54 -0400

    Agregando botones

---
+++ Duke4/CXC/Proceso/Dateprocesoprueba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Duke4.CXC.Proceso
+{
+    public partial class Dateprocesoprueba : Duke4.MainFormProceso
+    {
+        public Dateprocesoprueba()
+        {
+            InitializeComponent();
+        }
+    }
+}
